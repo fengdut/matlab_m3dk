b7b6ae2ac9de8d85c01c11a5315ea64938030323
Wang Feng <fengwang85@gmail.com>
2015-09-08 15:30:12 -0400

    new file:   startup.m matlab startup.m configure file example

---
+++ startup.m
@@ -0,0 +1,16 @@
+
+
+
+% Disp('add or copy this file to your matlab default startup folder');
+% Disp('and change matlab_m3dk_path to your local path');
+
+set(0,'DefaultTextInterpreter','latex');
+set(0,'DefaultAxesFontSize',18);
+set(0,'DefaultAxesFontName','Latin Modern Roman');
+
+matlab_m3dk_path='/Users/fwang/matlab_m3dk';
+addpath(matlab_m3dk_path);
+
+cd /Users/fwang/Documents/work;
+
+
