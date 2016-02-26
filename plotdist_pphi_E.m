0bd68519e8248f9d10ac0c8dda3799e5bbc5ec71
Wang Feng <fengwang85@gmail.com>
2016-02-26 08:45:06 -0500

    new file:   plotdist_pphi_E.m

---
+++ plotdist_pphi_E.m
@@ -0,0 +1,35 @@
+function [pphi,fpphi,pangle,fpangle,e,fe]=plotdist_pphi_E(filename,j)
+[pphi,dpphi,pangle,dpangle,e,de,f,f0]=getdistf_cdf(filename);
+
+
+fpphi_e(1:size(pphi,1),1:size(e,1))=0;
+fpphi_e0(1:size(pphi,1),1:size(e,1))=0;
+
+
+clf;
+set(gcf,'Units','points','position',[100 100 800 600],'Color',[1 1 1]);
+hax=axes('Position',[0.14 0.14 0.75 0.75],'FontSize',24); 
+
+[X,Y]=meshgrid(pphi,e);
+
+size(pangle,1)
+
+for i=1:size(pphi,1)
+    for k=1:size(e,1);
+        fpphi_e(i,k)=f(k,j,i);
+        fpphi_e0(i,k)=f0(k,j,i);        
+    end
+end
+
+contour(X,Y,fpphi_e',100);
+xlabel('$P_\phi$','Interpreter','latex');
+ylabel('$E$','Interpreter','latex');
+
+titles=sprintf('$\\mu_0=%2.3f, \\Delta \\mu=%2.3f$',pangle(j),dpangle);
+title(titles);
+
+grid on;
+
+
+
+
